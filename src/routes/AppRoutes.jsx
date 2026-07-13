bb97b40e5b77c8fbd7cfde5b72cb5f17111bc901
syniahpeterson <syniahpeterson@gmail.com>
2026-07-13 12:26:20 -0400

    Initial project setup

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from '../pages/Home'
+import About from '../pages/About'
+import Services from '../pages/Services'
+import ServiceDetail from '../pages/ServiceDetail'
+import Contact from '../pages/Contact'
+import FAQ from '../pages/FAQ'
+import NotFound from '../pages/NotFound'
+
+export default function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
+      <Route path="/services/:slug" element={<ServiceDetail />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/faq" element={<FAQ />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
+}
